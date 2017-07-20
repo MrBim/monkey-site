f69315555b0397e418e46454630f8bfc24f79113
MrBim <bimtech84@gmail.com>
2017-07-20 16:29:44 +0100

    test

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ angular.module('MonkeySite', ['ngRoute', 'RouteControllers','AlbumDirective']);
 angular.module('MonkeySite').config(function($locationProvider, $routeProvider){
     $locationProvider.html5Mode(true);
 
-	$routeProvider.when('monkey-site/',{
+	$routeProvider.when('/',{
 		templateUrl: 'monkey-site/templates/home.html',
 		controller: 'HomeController'
 	})
