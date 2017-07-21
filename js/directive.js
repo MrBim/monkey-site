9a92e9b41d7315d6aeec7ef71c91a1d4ad9892fa
MrBim <bimtech84@gmail.com>
2017-07-21 22:12:23 +0100

    href

--- js/directive.js
+++ js/directive.js
@@ -1,6 +1,6 @@
 angular.module('AlbumDirective',[]).directive('albums', function() {
   return {
     restrict: 'E',    // A -> attribute
-    templateUrl: 'monkey-site/templates/directives/albums.html'
+    templateUrl: 'templates/directives/albums.html'
   };
 });  
