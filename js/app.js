0521e88dca328e4b54bd7c3d5cfa3ebcb8ec0db1
MrBim <bimtech84@gmail.com>
2017-07-21 22:09:07 +0100

    href

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@ angular.module('MonkeySite', ['ngRoute', 'RouteControllers','AlbumDirective']);
 angular.module('MonkeySite').config(function($locationProvider, $routeProvider){
     $locationProvider.html5Mode(true);
 
-	$routeProvider.when('monkey-site/',{
+	$routeProvider.when('.',{
 		templateUrl: 'monkey-site/templates/home.html',
 		controller: 'HomeController'
 	})
-	.when('/about',{
+	.when('about',{
 		templateUrl: 'monkey-site/templates/about.html',
 		controller: 'AboutController'
 	})
-	.when('/gigs', {
+	.when('gigs', {
 		templateUrl: 'monkey-site/templates/gigs.html',
 		controller: 'GigsController'
 	})
-	.when('/media', {
+	.when('media', {
 		templateUrl: 'monkey-site/templates/media.html',
 		controller: 'MediaController'
 	})
-	.when('/contact', {
+	.when('contact', {
 		templateUrl: 'monkey-site/templates/contact.html',
 		controller: 'ContactController'
 	});
